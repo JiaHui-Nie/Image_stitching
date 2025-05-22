9f310584cd91fde4a8950d76e24036a852bdfab2
JiaHui-Nie <63399415+JiaHui-Nie@users.noreply.github.com>
2025-05-22 22:27:44 +0800

    优化性能
    进一步使用numpy优化性能，本次大约是上个版本耗时的15%

--- main.py
+++ main.py
@@ -26,11 +26,10 @@ def processImages(filelist):
         aver = np.abs(arr.mean(axis=2)-128)
 
         operate = np.less(aver, current_aver)
-        for j in range(i.size[1]):
-            for k in range(i.size[0]):
-                if operate[j][k]:
-                    current_arr[j][k] = arr[j][k]
-                    current_aver[j][k] = aver[j][k]
+        np.copyto(current_aver, aver, where=operate)
+        operate = operate.repeat(3)
+        operate.resize(arr.shape)
+        np.copyto(current_arr, arr, where=operate)
     output = Image.fromarray(current_arr)
     output.save("output.png")
 
